101bd35198c4d11b4b4c34d2b8715c6181b0c50a
Rihards Veters <rihardsveters@Rihardss-MacBook-Pro.local>
2018-11-30 08:24:42 +0200

    Something doesnt work here. After few times of refresh and reload browser it crashes

---
+++ src/app/card/card-listing/card-listing.page.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoaderService } from '../../shared/service/loader.service';
+import { CardService } from '../shared/card.service';
+import { ToastService } from '../../shared/service/toast.service';
+import { Card } from '../shared/card.model';
+
+@Component({
+  selector: 'app-card-listing',
+  templateUrl: './card-listing.page.html',
+  styleUrls: ['./card-listing.page.scss'],
+})
+export class CardListingPage {
+
+  cardDeckGroup: string;
+  cardDeck: string;
+  cards: Card[] = [];
+
+
+  constructor(private route: ActivatedRoute, 
+              private cardService : CardService,
+              private loaderService: LoaderService,
+              private toaster: ToastService) { }
+
+  private getCards(){
+    this.loaderService.presentLoading();
+    this.cardService.getCardsByDeck(this.cardDeckGroup, this.cardDeck).subscribe(
+      (cards: Card[]) => {
+        
+        this.cards = cards.map((card: Card) => {
+          card.text = this.cardService.replaceCardTextLine(card.text);
+          return card;
+        });
+        this.loaderService.dismissLoading();
+      }, () => {
+        this.loaderService.dismissLoading();
+        this.toaster.presentErrorToast("Ups, cards could not be loaded, try to refresh page")
+      })
+  }
+  
+  ionViewWillEnter(){
+    this.cardDeckGroup = this.route.snapshot.paramMap.get('cardDeckGroup');
+    this.cardDeck = this.route.snapshot.paramMap.get('cardDeck');
+
+    if(this.cards && this.cards.length === 0) this.getCards();
+  }
+  
+  doRefresh(event){
+    this.getCards();
+    event.target.complete();
+  }
+}
---
+++ src/app/card/card.module.ts
@@ -0,0 +1,34 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CardDeckPage } from './card-deck/card-deck.page';
+import { CardListingPage } from './card-listing/card-listing.page';
+import { CardDetailPage } from './card-detail/card-detail.page';
+
+import { CardService } from './shared/card.service';
+import { LoaderService } from '../shared/service/loader.service';
+import { ToastService } from '../shared/service/toast.service';
+
+import { CardListComponent } from './components/card-list.component';
+
+@NgModule({
+    imports: [
+        IonicModule,
+        CommonModule,
+        HttpClientModule
+    ],
+    providers: [
+        CardService,
+        LoaderService,
+        ToastService
+    ],
+    declarations:[
+        CardDeckPage,
+        CardListingPage,
+        CardDetailPage,
+        CardListComponent
+    ]
+})
+export class CardPageModule{}
